9971c0a9ce068ac02d248e91cae35dd2fe7d0975
ArtioG <79917723+ArtioG@users.noreply.github.com>
2023-08-10 10:57:03 -0300

    Add files via upload

---
+++ atividade1/variaveisOperadores.java
@@ -0,0 +1,14 @@
+public class variaveisOperadores {
+    public static void main(String[]args){
+        int NumA = 2 , NumB = 2, OPSm, OPSb, OPMt, OPDv;
+        OPSm = NumA + NumB;
+        OPSb = NumA - NumB;
+        OPMt = NumA * NumB;   
+        OPDv = NumA / NumB;
+        System.out.println("Resultado da operacao de soma entra NumA e NumB e de: " +OPSm);
+        System.out.println("Resultado da operacao de subtacao entra NumA e NumB e de: " +OPSb);
+        System.out.println("Resultado da operacao de multiplicao entra NumA e NumB e de: " +OPMt);
+        System.out.println("Resultado da operacao de divisao entra NumA e NumB e de: " +OPDv);
+
+    }
+}
